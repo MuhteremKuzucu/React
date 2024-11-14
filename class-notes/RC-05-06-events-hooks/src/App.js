7b47dfefabbe47d1951210e274e38f7acd960bec
MuhteremKuzucu <kuzucumuhterem@gmail.com>
2024-11-14 09:27:37 +0100

    5 finisch

---
+++ class-notes/RC-05-06-events-hooks/src/App.js
@@ -0,0 +1,27 @@
+import Events from "./1-events-hooksIntro/Events";
+import Counter from "./2-classComponent-state/Counter";
+import Hooks from "./2-functionalComponent-usestate/Hooks";
+import Form from "./3-Form/Form";
+import FormObject from "./3-Form/FormObject";
+import MouseKeyboard from "./4-mouse-keyboard/MouseKeyboard";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div >
+      {/* <Events/> */}
+
+      {/* <Hooks/> */}
+
+      {/* <Counter/> */}
+
+      {/* <Form/> */}
+<FormObject/>
+
+
+      {/* <MouseKeyboard/> */}
+    </div>
+  );
+}
+
+export default App;
